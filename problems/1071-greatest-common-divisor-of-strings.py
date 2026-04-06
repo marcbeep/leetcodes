c0907370cf18243c85daa07be56ae3141af4d6a8
Marc Beepath <marcbeep@gmail.com>
2026-04-06 18:07:03 +0100

    descriptions

--- problems/1071-greatest-common-divisor-of-strings.py
+++ problems/1071-greatest-common-divisor-of-strings.py
@@ -1,7 +1,8 @@
 """
 tag: array
 difficulty: easy
-link: https://leetcode.com/problems/greatest-common-divisor-of-strings
+link: https://leetcode.com/problems/greatest-common-divisor-of-strings/
+description: For two strings s and t, we say "t divides s" if and only if s = t + t + t + ... + t + t (i.e., t is concatenated with itself one or more times). Given two strings str1 and str2, return the largest string x such that x divides both str1 and str2.
 hint: If a GCD string exists, str1 + str2 equals str2 + str1; then take the prefix of length gcd(|str1|, |str2|).
 """
 
--- problems/1768-merge-strings-alternately.py
+++ problems/1768-merge-strings-alternately.py
@@ -1,7 +1,8 @@
 """
 tag: array
 difficulty: easy
-link: https://leetcode.com/problems/merge-strings-alternately
+link: https://leetcode.com/problems/merge-strings-alternately/
+description: You are given two strings word1 and word2. Merge the strings by adding letters in alternating order, starting with word1. If a string is longer than the other, append the additional letters onto the end of the merged string. Return the merged string.
 hint: Use two pointers and alternate between each string until both are exhausted.
 """
 
